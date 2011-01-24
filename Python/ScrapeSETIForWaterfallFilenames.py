8bce4e8bfb10d3d8bd29d876c60a350fcfefc9b7
Francis Potter <git@fpotter.com>
2011-01-23 16:03:49 -0800

    Actual waterfall plot URLs for 3 stars, all hard-coded.
    Signed-off-by: Francis Potter <git@fpotter.com>

--- Python/ScrapeSETIForWaterfallFilenames.py
+++ Python/ScrapeSETIForWaterfallFilenames.py
@@ -2,44 +2,51 @@
 from urllib import urlopen
 from re import compile
 
+targets = {'tauCeti': "2010-11-06-dorothy_tauceti_1420_1",
+ 				'epsilonEridani': "2010-11-06-dorothy_epsiloneridani_1420_1",
+				'gliese581': "2010-11-05-dorothy_exo-gl581-1420_1"}
 
-targetCode = "2010-11-06-dorothy_tauceti_1420_1"
 
 
 directoryURLFormat = "http://184.73.240.15/analysis/%s/images/chan%s/"
 
 
 
+waterfallTileActionscriptFormat = '%sObservation.addWaterfallTile(new WaterfallTile(%s, %s, "%s"));\n'
 
-waterfallTileActionscriptFormat = 'observation.addWaterfallTile(new WaterfallTile(%s, %s, "%s"));\n'
-waterfallTileActionScript = ""
 
-for channel in range (-9, 9):
+outputFile = open("OutputFromSETIScraper.as", "w")
+
+for target in targets:
 	
-	print targetCode, channel;
+	targetCode = targets[target]
+
+	for channel in range (-9, 9):
 	
-	channelString = "%(channel)+05d" % {'channel':channel}
-	directoryURL = directoryURLFormat % (targetCode, channelString)
-	directoryStream = urlopen(directoryURL)
+		print targetCode, channel;
+	
+		channelString = "%(channel)+05d" % {'channel':channel}
+		directoryURL = directoryURLFormat % (targetCode, channelString)
+		directoryStream = urlopen(directoryURL)
 
-	directoryHTML = directoryStream.read()
-	directoryStream.close()
+		directoryHTML = directoryStream.read()
+		directoryStream.close()
 
-	# testString = '<a href="chan+0000-1-1419.999201-1419.999467.png">'
+		# testString = '<a href="chan+0000-1-1419.999201-1419.999467.png">'
 
-	filenameRegExpString = '<a href="(.*?\.png)">'
-	filenameRegExp = compile(filenameRegExpString)
+		filenameRegExpString = '<a href="(.*?\.png)">'
+		filenameRegExp = compile(filenameRegExpString)
 
-	filenamesForChannel = filenameRegExp.findall(directoryHTML)
+		filenamesForChannel = filenameRegExp.findall(directoryHTML)
 
-	for filename in filenamesForChannel:
+		for filename in filenamesForChannel:
 		
-		minWavelengthString = filename[12:23]
-		maxWavelengthString = filename[24:35]
+			minWavelengthString = filename[12:23]
+			maxWavelengthString = filename[24:35]
 		
-		waterfallTileActionScript += waterfallTileActionscriptFormat % (minWavelengthString, maxWavelengthString, directoryURL + filename)
+			outputFile.write(waterfallTileActionscriptFormat % (target, minWavelengthString, maxWavelengthString, directoryURL + filename))
 
-print waterfallTileActionScript
+outputFile.close()
 
 
 
