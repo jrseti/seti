d541cf40a81165908a82c27df2ea70cd71854bde
Francis Potter <git@fpotter.com>
2011-02-20 23:46:27 -0800

    Starting a Ruby on Rails project. Fairly certain this is the right API framework to use.

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seti::Application.config.secret_token = 'dd540f9d9c7fc229bc68f80235c1e168728f354a22d80c1530496343a7f363d6d26961378bcfe871b91d04ff1073230d9af9e0c668efadd1a2c90e9b460dc658'
